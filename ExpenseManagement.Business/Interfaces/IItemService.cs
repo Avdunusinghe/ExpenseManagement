82b4262dad6c7f8c61a0bfcaea7b5dca75c6d55e
Avdunusinghe <avdunusinghe@gmail.com>
2021-11-05 22:24:28 +0530

    Get All item Service Implemeted

---
+++ ExpenseManagement.Business/Interfaces/IItemService.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManagement.Business.Interfaces
+{
+    public interface IItemService
+    {
+        List<Item> GetAllItems();
+    }
+}
---
+++ ExpenseManagement.Business/ItemService.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseManagement.Business.Interfaces;
+using ExpenseManagement.Data.Data;
+using ExpenseManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManagement.Business
+{
+    public class ItemService : IItemService
+    {
+        private readonly ExpenseManagementDbContext _db;
+        public ItemService(ExpenseManagementDbContext _db)
+        {
+            this._db = _db;
+        }
+
+        public List<Item> GetAllItems()
+        {
+            var response = new List<Item>();
+
+            var itemList = _db.Items;
+
+            foreach(var items in itemList)
+            {
+                response.Add(items);
+            }  
+
+            return response;
+        }
+    }
+}
