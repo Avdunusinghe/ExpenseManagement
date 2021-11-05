490bc0a2569b8ae3d572ba272ec2b5ed6c85ab51
Avdunusinghe <avdunusinghe@gmail.com>
2021-11-05 14:26:00 +0530

    Configure Services

---
+++ ExpenseManagement.Data/Data/ExpenseManagementDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManagement.Data.Data
+{
+    public class ExpenseManagementDbContext : DbContext
+    {
+        public ExpenseManagementDbContext(DbContextOptions<ExpenseManagementDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-2576JIPH\\SQLEXPRESS;Database=ExpenseManagement;User Id=sa;Password=1qaz2wsx@;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- ExpenseManagement.WebService/Startup.cs
+++ ExpenseManagement.WebService/Startup.cs
@@ -1,6 +1,8 @@
+using ExpenseManagement.Data.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,8 +25,25 @@ namespace ExpenseManagement.WebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ExpenseManagementDbContext>(option =>
+
+                option.UseSqlServer(Configuration.GetConnectionString("ConnectionString"))
+            );
+
+            services.AddEntityFrameworkSqlServer().AddDbContext<ExpenseManagementDbContext>(options =>
+            {
+                options.UseLazyLoadingProxies()
+                .UseSqlServer(Configuration["ConnectionString"],
+                              sqlServerOptionsAction: sqlOptions =>
+                              {
+                                  sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                              });
+            });
+
             services.AddControllersWithViews();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
