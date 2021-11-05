2a9975cbd4ef9e30fd545bb4bf63e0d7e340e10b
Avdunusinghe <avdunusinghe@gmail.com>
2021-11-05 20:34:33 +0530

    Item View Partlly Impemented

---
+++ ExpenseManagement.WebService/Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseManagement.Data.Data;
+using ExpenseManagement.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManagement.WebService.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ExpenseManagementDbContext _db;
+        public ItemController(ExpenseManagementDbContext _db)
+        {
+            this._db = _db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> items = _db.Items;
+            return View(items);
+        }
+    }
+}
